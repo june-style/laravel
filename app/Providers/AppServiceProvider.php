30121edb05584f685382d9ec643409f683c9d673
june-style <github@cryberry-jc.net>
2017-10-27 16:20:21 +0900

    学習用

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // For laralib/l5scaffold
+        if ($this->app->environment() == 'local') {
+            $this->app->register('Laralib\L5scaffold\GeneratorsServiceProvider');
+        }
+    }
+}
